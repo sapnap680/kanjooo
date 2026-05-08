59a41aac33aa50fca33fa39b3e763e67a4c7b279
sapnap680 <02057478@keio.jp>
2026-05-08 19:31:52 +0900

    Add files via upload

---
+++ だれのこと - コピー/firebase-config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyAlvWdgLjvDJAWi9TGG1zbUCiRDQYUoX54",
+  authDomain: "kanjo-90a6e.firebaseapp.com",
+  projectId: "kanjo-90a6e",
+  storageBucket: "kanjo-90a6e.firebasestorage.app",
+  messagingSenderId: "685954506264",
+  appId: "1:685954506264:web:cebde6c1eb7d25dbe92d6d"
+};
